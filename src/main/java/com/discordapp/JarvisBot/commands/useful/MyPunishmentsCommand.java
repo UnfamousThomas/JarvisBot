eba13183bfc98d87ebd2e045326f3ef7720d1623
ThomasHere123 <thom.palts@gmail.com>
2020-01-08 17:27:34 +0300

    Remove space aka test webhook

--- src/main/java/com/discordapp/JarvisBot/commands/useful/MyPunishmentsCommand.java
+++ src/main/java/com/discordapp/JarvisBot/commands/useful/MyPunishmentsCommand.java
@@ -33,11 +33,10 @@ public class MyPunishmentsCommand extends Command {
 
         if(!manager.userPunishments.get(m.getIdLong()).isEmpty()) {
             List<PunishmentObject> objects = new ArrayList<>(manager.userPunishments.get(m.getIdLong()));
-            
+
             m.getUser().openPrivateChannel().queue(channel -> channel.sendMessage(makeEmbed(objects, event.getGuild(), false).build()).queue());
         } else {
             m.getUser().openPrivateChannel().queue(channel -> channel.sendMessage(makeEmbed(null, event.getGuild(), true).build()).queue());
-
         }
 
     }
