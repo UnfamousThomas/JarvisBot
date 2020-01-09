978dc0bfc3b897cada2700425ff6adb85e046361
ThomasHere123 <thom.palts@gmail.com>
2020-01-09 11:59:00 +0200

    Update AppealCommand.java

--- src/main/java/com/discordapp/JarvisBot/commands/admin/AppealCommand.java
+++ src/main/java/com/discordapp/JarvisBot/commands/admin/AppealCommand.java
@@ -1,6 +1,8 @@
 package com.discordapp.JarvisBot.commands.admin;
 
+import com.discordapp.JarvisBot.commands.api.Categories;
 import com.discordapp.JarvisBot.commands.api.Command;
+import com.discordapp.JarvisBot.utils.CustomPermission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -9,6 +11,10 @@ import java.util.List;
 public class AppealCommand extends Command {
     public AppealCommand() {
         super("appeal");
+        minArgs = 2;
+        maxArgs = 2;
+        permission = CustomPermission.ADMIN;
+        category = Categories.ADMIN;
     }
 
     @Override
